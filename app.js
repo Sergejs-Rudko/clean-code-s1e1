d172dede067baece640d55de9f3729fa3f936f73
Sergejs-Rudko <sergejs.rudko@gmail.com>
2023-04-22 22:08:20 +0100

    refactor: selector separation according to rule basic 3.8

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
+    var checkBox=taskListItem.querySelector(".item__chk");
     var editButton=taskListItem.querySelector(".button-edit");
     var deleteButton=taskListItem.querySelector(".button-delete");
 
